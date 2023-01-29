2de8d21776920f4c4ee5e7c96d554969f04eef2d
OneWinterSigh <cabelloruizgerman@gmail.com>
2023-01-29 19:12:14 +0100

    up

--- App.js
+++ App.js
@@ -1,7 +1,34 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 export default function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true); //This is a hook 
+  const [user, setUser] = useState(); //This is another hook
+
+  //Handle user state changes
+  const onAuthStateChanged = (user) => {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; //unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if(!user){
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
